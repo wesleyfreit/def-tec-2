fe5789f506ce8154429250073c2c72e27c6fcd78
RICHARD WESLEY BARBOSA FREITAS <wesleyrichard30@gmail.com>
2023-11-21 20:46:57 -0300

    configurando o middleware de validação do token jwt

--- src/middlewares/userAuth.ts
+++ src/middlewares/userAuth.ts
@@ -1,5 +1,26 @@
 import { NextFunction, Request, Response } from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 
 export const userAuth = async (req: Request, res: Response, next: NextFunction) => {
-  next();
+  const authorization = req.headers.authorization?.replace('Bearer ', '') as string;
+  const secret = process.env.JWT_SECRET as string;
+
+  jwt.verify(authorization, secret, (err, decoded) => {
+    if (err) {
+      switch (err.message) {
+        case 'jwt must be provided':
+          return res.status(400).json({ mensagem: 'Não autorizado.' });
+        case 'jwt expired':
+          return res.status(400).json({ mensagem: 'Sessão Inválida.' });
+        default:
+          return res.status(500).json({ messagem: 'Ocorreu um erro interno no servidor.' });
+      }
+    } else {
+      const { id } = req.params;
+      const payload = decoded as JwtPayload;
+
+      if (payload.id === id) next();
+      else return res.status(400).json({ mensagem: 'Não autorizado.' });
+    }
+  });
 };
