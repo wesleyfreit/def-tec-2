f3c0c1c01f309541e8952603b6ce5b21ecefe7be
RICHARD WESLEY BARBOSA FREITAS <wesleyrichard30@gmail.com>
2023-11-22 11:07:34 -0300

    adicionando instâncias de newUser em cada teste para evitar erros de primary key

--- __tests__/User.spec.ts
+++ __tests__/User.spec.ts
@@ -2,16 +2,16 @@ import { User } from '../src/models/User';
 
 const userModel = new User();
 
-const newUser = {
-  nome: 'Romulo Silva',
-  email: 'emailofwho@gmail.com',
-  senha: '123456',
-  telefones: [{ numero: '15493189', ddd: '82' }],
-};
-
 describe('Serviço de usuários', () => {
   it('Deve ser possível cadastrar um usuário', async () => {
     const usersBefore = await userModel.getAllUsers();
+
+    const newUser = {
+      nome: 'Romulo Silva',
+      email: 'rmdslv@gmail.com',
+      senha: '$12341dsa556%',
+      telefones: [{ numero: '15493189', ddd: '82' }],
+    };
 
     const userCreated = await userModel.createNewUser(
       newUser.nome,
@@ -28,6 +28,13 @@ describe('Serviço de usuários', () => {
   });
 
   it('Deve ser possível recuperar um usuário pelo email', async () => {
+    const newUser = {
+      nome: 'Ronaldo Ferreira',
+      email: 'rondlfr@gmail.com',
+      senha: '54@9zd24714',
+      telefones: [{ numero: '45415479', ddd: '22' }],
+    };
+
     const userCreated = await userModel.createNewUser(
       newUser.nome,
       newUser.email,
@@ -43,6 +50,13 @@ describe('Serviço de usuários', () => {
   });
 
   it('Deve ser possível validar se a senha fornecida corresponde à senha do usuário', async () => {
+    const newUser = {
+      nome: 'Maria Santana',
+      email: 'mrast@gmail.com',
+      senha: '54926gfg814',
+      telefones: [{ numero: '45924792', ddd: '15' }],
+    };
+
     const userCreated = await userModel.createNewUser(
       newUser.nome,
       newUser.email,
@@ -63,6 +77,13 @@ describe('Serviço de usuários', () => {
   });
 
   it('Deve ser possível recuperar o usuário através do id dele', async () => {
+    const newUser = {
+      nome: 'Ana Dias',
+      email: 'andias@gmail.com',
+      senha: '14acz989245',
+      telefones: [{ numero: '45415479', ddd: '35' }],
+    };
+
     const userCreated = await userModel.createNewUser(
       newUser.nome,
       newUser.email,
@@ -78,6 +99,13 @@ describe('Serviço de usuários', () => {
   });
 
   it('Deve ser possível atualizar o último login do usuário', async () => {
+    const newUser = {
+      nome: 'João Gonçalves',
+      email: 'jrgca@gmail.com',
+      senha: 'dsa445d5s',
+      telefones: [{ numero: '54148192', ddd: '83' }],
+    };
+
     const userCreatedAndFirstLogin = await userModel.createNewUser(
       newUser.nome,
       newUser.email,
